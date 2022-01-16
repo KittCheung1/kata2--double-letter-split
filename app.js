8bc8feb3ff7ee3d2123e30af05e241c982d81e7a
Kitt Cheung <kitt.cheung@live.se>
2022-01-16 17:13:12 +0100

    my version of solution added

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-function splitOnDoubleLetter(input){
-    let start = 0;
-    let word = "";
-    let arr =[];
+// function splitOnDoubleLetter(input){
+//     let start = 0;
+//     let word = "";
+//     let arr =[];
 
-    for (let i = 0; i < input.length; i++) {
-        let temp = i+1;
-        if (input[i] === input[temp]) {
-            arr.push(input.slice(start, temp));
-            word = input.slice(temp, input.length);
-            start = temp; 
+//     for (let i = 0; i < input.length; i++) {
+//         let temp = i+1;
+//         if (input[i] === input[temp]) {
+//             arr.push(input.slice(start, temp));
+//             word = input.slice(temp, input.length);
+//             start = temp; 
+//         }
+//     }
+//     arr.push(word);
+//     console.log(arr);
+// }
+
+
+function splitOnDoubleLetter(input) {
+    let strings = "";
+    let arr = [];
+
+    for (let char in input) {
+        if (input[char] == input[char - 1]) {
+            strings += " ";
         }
+        strings += input[char];
     }
-    arr.push(word);
+    arr = strings.split(" ");
     console.log(arr);
+
 }
 
 splitOnDoubleLetter("letterrs");
+splitOnDoubleLetter("mississippi");
